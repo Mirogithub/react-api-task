b00d88d6f3845bc48bbbb45d3e17df5bbe1fe3f6
Mirogithub <45472039+Mirogithub@users.noreply.github.com>
2019-05-05 23:06:45 +0300

    Update swapi-service.js

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -31,12 +31,6 @@ export default class SwapiService{
 ///////*****************************///////////////////////////
 
 
-  // async getFilm(id=0){
-  //   const film = await this.getResourse(`&page=1`)
-  //   console.log(film.results[id])
-  //   return this._transformFilm(film.results[id])
-  // }
-
   getFilm= async(id)=>{
     const film = await this.getResourse(`&page=1`)
     return film.results.filter((item)=>{
@@ -58,13 +52,6 @@ export default class SwapiService{
   }
 }
 
-// const swapi = new SwapiService();
-// swapi.getAllFilms().then((body)=>{
-//   console.log(body)
-// })
-// swapi.getFilm(2).then((body)=>{
-//   console.log(body)
-// })
 
 
 
@@ -114,31 +101,3 @@ export default class SwapiService{
 
 
 
-
-
-
-
-// const getResourse = async (url) =>{
-//   const res = await fetch(url);
-//   if(!res.ok){
-//     throw new Error(`Could not fetch ${url} + received ${res.status}`)
-//   }
-//   return await res.json();;
-// }
-
-
-// getResourse('https://api.themoviedb.org/3/movie/popular?api_key=6c9632b0adb4a4ce0ca5452b8dd00356&language=en-US&page=1')
-// .then((body)=>{
-//   console.log(body.results[2])
-// }).catch((err)=>{
-//   console.error('Can\'t get data', err)
-// })
-
-
-// fetch('https://api.themoviedb.org/3/movie/popular?api_key=6c9632b0adb4a4ce0ca5452b8dd00356&language=en-US&page=1')
-// .then((res)=>{
-//   return res.json();
-//   // console.log('Got Response', res.json());
-// }).then((body)=>{
-//   console.log(body)
-// })
